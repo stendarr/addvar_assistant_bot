60c1456d6170fdd199a3441b217070a7ea7b9b89
stendarr <odrljind@vis.ethz.ch>
2019-02-09 01:54:25 +0100

    Initial commit

---
+++ meinkraft.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd ~/meinkraft && nohup java -Xms1024M -Xmx4G -jar ./server.jar nogui &
---
+++ minecraft.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd ~/minecraft && nohup java -Xms1024M -Xmx7G -jar ./server.jar nogui &
